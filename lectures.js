67e37958425e9a599b7daa1394f963512a8db3ce
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-21 17:09:06 +0200

    Implementing tabbed component, activate  tab, remove classes

--- lectures.js
+++ lectures.js
@@ -152,6 +152,7 @@ document.querySelector(".nav").addEventListener(
 );
 */
 
+/*
 ///////////////////////////////////////////////////////////
 // DOM TRAVERSING
 console.log("--------------EVENTS BUBBLING--------------");
@@ -184,3 +185,4 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = "scale(0.5)";
 });
+*/
--- script.js
+++ script.js
@@ -52,7 +52,7 @@ btnScrollTo.addEventListener("click", function (e) {
     });*/
 
   // New way work on new browsers
-  section1.scrollIntoView({ behavior: "smooth" });
+  section1.scrollIntoView({behavior: "smooth"});
 });
 
 ///////////////////////////////////////////////////////////
@@ -78,6 +78,32 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   // Matching strategy
   if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    document.querySelector(id).scrollIntoView({behavior: "smooth"});
   }
+});
+
+///////////////////////////////////////////////////////////
+// TABBED COMPONENT
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // Activate tab
+  clicked.classList.add("operations__tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
 });
