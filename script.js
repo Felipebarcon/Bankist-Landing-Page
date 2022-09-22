34a56b5907c822a786a1238d302cc68729f22979
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-22 11:46:41 +0200

    slides right to left work

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const nav = document.querySelector(".nav");
 const header = document.querySelector(".header");
 const allSections = document.querySelectorAll(".section");
 const imgTargets = document.querySelectorAll("img[data-src]");
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
 
 ///////////////////////////////////////
 // Modal window
@@ -59,7 +62,7 @@ btnScrollTo.addEventListener("click", function (e) {
     });*/
 
   // New way work on new browsers
-  section1.scrollIntoView({behavior: "smooth"});
+  section1.scrollIntoView({ behavior: "smooth" });
 });
 
 ///////////////////////////////////////////////////////////
@@ -85,7 +88,7 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   // Matching strategy
   if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({behavior: "smooth"});
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
 
@@ -177,7 +180,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add("section--hidden");
+  //section.classList.add("section--hidden");
 });
 
 ///////////////////////////////////////////////////////////
@@ -191,16 +194,54 @@ const loadImg = function (entries, observer) {
   // Replace src with data-src
   entry.target.src = entry.target.dataset.src;
 
-  entry.target.addEventListener('load', function () {
-    entry.target.classList.remove('lazy-img')
-  })
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
 
-  observer.unobserve(entry.target)
+  observer.unobserve(entry.target);
 };
 
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
+  rootMargin: "-200px",
 });
 
 imgTargets.forEach((img) => imgObserver.observe(img));
+
+///////////////////////////////////////////////////////////
+// SLIDER COMPONENT
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
