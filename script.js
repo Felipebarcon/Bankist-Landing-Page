c16f808fc4123ea560c54b88c4539a2c51956f17
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-22 09:39:57 +0200

    sticky navbar firt attempt don't work

--- script.js
+++ script.js
@@ -126,3 +126,12 @@ const handleHover = function (e) {
 // Passing 'argument' into handler
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+///////////////////////////////////////////////////////////
+// STICKY NAV BAR
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > initialCoords) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+});
