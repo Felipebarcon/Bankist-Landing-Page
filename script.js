0aaa0a2e5d7d01ce9cc59373117a6a42f4df1526
Felipe Barcon <felipe.barcon@gmail.com>
2022-09-22 08:51:58 +0200

    Menu fade animation function

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav");
 
 ///////////////////////////////////////
 // Modal window
@@ -52,7 +56,7 @@ btnScrollTo.addEventListener("click", function (e) {
     });*/
 
   // New way work on new browsers
-  section1.scrollIntoView({behavior: "smooth"});
+  section1.scrollIntoView({ behavior: "smooth" });
 });
 
 ///////////////////////////////////////////////////////////
@@ -78,16 +82,12 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   // Matching strategy
   if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({behavior: "smooth"});
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
 
 ///////////////////////////////////////////////////////////
 // TABBED COMPONENT
-
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
 
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
@@ -107,3 +107,22 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+///////////////////////////////////////////////////////////
+// MENU FADE ANIMATION
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing 'argument' into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
